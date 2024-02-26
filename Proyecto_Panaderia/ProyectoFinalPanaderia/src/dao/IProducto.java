87b68b5fb44b76627648cfd900c2db6d556de5a7
PIPE <PIPE@LAPTOP-UHNB891L>
2024-02-26 18:45:02 -0500

    1.0.3

---
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/dao/IProducto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import modelo.Producto;
+
+/**
+ *
+ * @author PIPE
+ */
+public interface IProducto {
+    public List<Producto> ListaProductos();
+    public boolean InsetarProductos(Producto producto);
+   public boolean ActualizarProductos(Producto producto);
+    public boolean EliminarProductos(int idProduto);
+    public Producto BuscarProductos( int  idProduto);
+}
