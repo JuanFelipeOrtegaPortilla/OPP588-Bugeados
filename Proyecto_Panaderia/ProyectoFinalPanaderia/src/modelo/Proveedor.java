670f3395d8c746e2f0cbb8cb91946ec924191197
PIPE <PIPE@LAPTOP-UHNB891L>
2024-02-26 18:45:53 -0500

    1.0.8

---
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/modelo/Proveedor.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author PIPE
+ */
+public class Proveedor {
+    private int idProveedor;
+    private String Proveedor;
+    private String marca;
+    private String nombreProducto; 
+    private int cantidad;
+    private Date fechaPedido;
+    private Date fechaPago;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(int idProveedor, String Proveedor, String marca, String nombreProducto, int cantidad, Date fechaPedido, Date fechaPago) {
+        this.idProveedor = idProveedor;
+        this.Proveedor = Proveedor;
+        this.marca = marca;
+        this.nombreProducto = nombreProducto;
+        this.cantidad = cantidad;
+        this.fechaPedido = fechaPedido;
+        this.fechaPago = fechaPago;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getProveedor() {
+        return Proveedor;
+    }
+
+    public void setProveedor(String Proveedor) {
+        this.Proveedor = Proveedor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Date getFechaPedido() {
+        return fechaPedido;
+    }
+
+    public void setFechaPedido(Date fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+
+    public Date getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(Date fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" + "idProveedor=" + idProveedor + ", Proveedor=" + Proveedor + ", marca=" + marca + ", nombreProducto=" + nombreProducto + ", cantidad=" + cantidad + ", fechaPedido=" + fechaPedido + ", fechaPago=" + fechaPago + '}';
+    }
+    
+
+}
