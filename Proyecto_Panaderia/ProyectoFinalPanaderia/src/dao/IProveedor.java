704893f6c3f66af284803277a5f72bbacd103c69
PIPE <PIPE@LAPTOP-UHNB891L>
2024-02-29 12:24:24 -0500

    1.7.0

---
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/dao/IProveedor.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import modelo.Proveedor;
+
+/**
+ *
+ * @author PIPE
+ */
+public interface IProveedor {
+
+    public List<Proveedor> ListaProveedor();
+
+    public boolean InsetarProveedor(Proveedor proveedor);
+
+    public boolean ActualizarProveedor(Proveedor proveedor);
+
+    public boolean EliminarProveedor(int idProveedor);
+
+    public Proveedor BuscarProveedor(int idProveedor);
+}
