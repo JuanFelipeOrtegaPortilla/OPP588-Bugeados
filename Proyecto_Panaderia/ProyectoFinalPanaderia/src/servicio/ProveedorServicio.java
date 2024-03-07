36860cbc0bee8c50e88ba4acce41ec19b8ab5a14
PIPE <PIPE@LAPTOP-UHNB891L>
2024-03-07 00:07:50 -0500

    2.1.5

--- Proyecto_Panaderia/ProyectoFinalPanaderia/src/servicio/ProveedorServicio.java
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/servicio/ProveedorServicio.java
@@ -4,7 +4,8 @@
  */
 package servicio;
 
-
+import dao.MetodosProveedor;
+import java.util.List;
 import modelo.Proveedor;
 
 /**
@@ -12,6 +13,21 @@ import modelo.Proveedor;
  * @author PIPE
  */
 public class ProveedorServicio {
-     //
 
+    public List<Proveedor> ListaProveedor() {
+        return new MetodosProveedor().ListaProveedor();
+    }
+
+    public boolean InsetarProveedor(Proveedor proveedor) {
+        return new MetodosProveedor().InsetarProveedor(proveedor);
+    }
+     public boolean ActualizarProveedor(Proveedor proveedor){
+         return new MetodosProveedor().ActualizarProveedor(proveedor);
+     }
+     public Proveedor BuscarProveedor(int idProveedor){
+         return new MetodosProveedor().BuscarProveedor(idProveedor);
+     }
+      public boolean ActualizarStock(Proveedor proveedor){
+          return new MetodosProveedor().ActualizarStock(proveedor);
+      }
 }
