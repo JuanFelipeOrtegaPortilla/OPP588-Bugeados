bd45aea73ce7bf806a669f30bacac7ff1fc55a21
PIPE <PIPE@LAPTOP-UHNB891L>
2024-03-06 23:02:41 -0500

    2.1.1

--- Proyecto_Panaderia/ProyectoFinalPanaderia/src/dao/MetodosProveedor.java
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/dao/MetodosProveedor.java
@@ -9,11 +9,14 @@ import com.mongodb.MongoException;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.result.UpdateResult;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import javax.swing.JOptionPane;
 import modelo.Clientes;
+
 import modelo.Proveedor;
 import org.bson.Document;
 
@@ -25,15 +28,17 @@ public class MetodosProveedor implements IProveedor {
 
     Conexion conn = new Conexion();
     MongoDatabase database;
-    private MongoCollection<Document> coleccion;
-    private MongoCollection<Document> coleccion2;
+    private MongoCollection<Document> coleccionProductos;
+    private MongoCollection<Document> coleccionProveedor;
+    private MongoCollection<Document> coleccionPerfil;
+    public List<Proveedor> ListaProveedor;
 
     public MetodosProveedor() {
         if (conn != null) {
             this.conn = conn.crearConexion();
             this.database = conn.getDataB();
-            this.coleccion = database.getCollection("proveedores");
-            this.coleccion2 = database.getCollection("productos");
+            this.coleccionProveedor = database.getCollection("proveedores");
+            this.coleccionProductos = database.getCollection("productos");
         }
     }
 
@@ -57,15 +62,15 @@ public class MetodosProveedor implements IProveedor {
         FindIterable<Document> documentos;
 
         try {
-            documentos = coleccion.find();
+            documentos = coleccionProveedor.find();
             for (Document temp : documentos) {
                 Proveedor proveedor = new Proveedor();
 
                 proveedor.setIdProveedor(generarID());
                 proveedor.setProveedor(temp.getString("nombreCliente"));
                 proveedor.setMarca(temp.getString("telefono"));
-                proveedor.setFechaPedido(temp.getDate("fechaCompra"));
-                proveedor.setFechaPago(temp.getDate("fechaPago"));
+                proveedor.setFechaPedido(temp.getString("fechaCompra"));
+                proveedor.setFechaPago(temp.getString("fechaPago"));
                 proveedor.setCancelado(temp.getBoolean("cancelado"));
 
                 listaProveedores.add(proveedor);
@@ -79,35 +84,118 @@ public class MetodosProveedor implements IProveedor {
 
         return listaProveedores;
     }
-    /*
-     private int idProveedor;
-    private String Proveedor;
-    private String marca;
-    private String nombreProducto;
-    private int cantidad;
-    private Date fechaPedido;
-    private Date fechaPago;
-    private boolean cancelado;
-    */
 
     @Override
     public boolean InsetarProveedor(Proveedor proveedor) {
+        Document documento;
+        try {
+            documento = new Document("id_proveedor", proveedor.getIdProveedor())
+                    .append("proveedor", proveedor.getProveedor())
+                    .append("marca", proveedor.getMarca())
+                    .append("nombreProducto", proveedor.getNombreProducto())
+                    //.append("cantidad", proveedor.getCantidad())
+                    .append("fechaPedido", proveedor.getFechaPedido())
+                    .append("fehchaPago", proveedor.getFechaPago())
+                    .append("listaEntrega",proveedor.getListaEntrega());
 
+            coleccionProveedor.insertOne(documento);
+        } catch (MongoException ex) {
+            JOptionPane.showMessageDialog(null, "Error al insertar datos: " + ex.toString());
+            return false;
+        } finally {
+            cerrarConexion();
+        }
+        return true;
     }
 
     @Override
     public boolean ActualizarProveedor(Proveedor proveedor) {
+        Document filtro, update;
+        UpdateResult resultado;
+        boolean actualizar = false;
+        try {
+        filtro = new Document("id_proveedor", proveedor.getIdProveedor());
+        update = new Document("$set", new Document("proveedor", proveedor.getProveedor())
+                .append("marca", proveedor.getMarca())
+                .append("nombreProducto", proveedor.getNombreProducto())
+                .append("fechaPago", proveedor.getFechaPago())); 
+        resultado = coleccionProveedor.updateOne(filtro, update);
 
+        if (resultado.getModifiedCount() > 0) {
+            actualizar = true;
+        }
+
+    } catch (Exception ex) {
+        JOptionPane.showMessageDialog(null, "Error al actualizar datos " + ex.getMessage());
+    } finally {
+        cierreConexion();
+    }
+        return actualizar;
     }
 
     @Override
     public Proveedor BuscarProveedor(int idProveedor) {
+    Proveedor proveedor = null;
+    Document filtro = null, resultado = null, perfil = null;
+    try {
+        filtro = new Document("id_proveedor", idProveedor);
+        resultado = coleccionProveedor.find(filtro).first();
 
+        if (resultado != null) {
+            proveedor = new Proveedor();
+            proveedor.setIdProveedor(resultado.getInteger("id_proveedor"));
+            proveedor.setProveedor(resultado.getString("proveedor"));
+            proveedor.setMarca(resultado.getString("marca"));
+            proveedor.setNombreProducto(resultado.getString("nombreProducto"));
+            proveedor.setFechaPedido(resultado.getString("fechaPedido"));
+            proveedor.setFechaPago(resultado.getString("fechaPago"));
+            proveedor.setCancelado(resultado.getBoolean("cancelado", false)); 
+
+            perfil = coleccionPerfil.find(eq("id_perfil", resultado.getInteger("id_perfil"))).first();
+            if (perfil != null) {
+               
+               proveedor.setNombrePerfil(perfil.getString("nombrePerfil"));
+            }
+        }
+    } catch (Exception ex) {
+        JOptionPane.showMessageDialog(null, "Error al consultar datos según ID: " + ex.getMessage());
+    } finally {
+        cierreConexion();
     }
+    return proveedor;
+}
+
+
 
     @Override
     public boolean ActualizarStock(Proveedor proveedor) {
+       
+    boolean actualizado = false;
+    
+    try {
+        Document filtro = new Document("id_proveedor", proveedor.getIdProveedor());
+        Document update = new Document("$set", new Document("cantidad", proveedor.getCantidad())); // Ajusta el nombre del campo según tu estructura
 
+        UpdateResult resultado = coleccionProveedor.updateOne(filtro, update);
+
+        if (resultado.getModifiedCount() > 0) {
+            actualizado = true;
+        }
+    } catch (Exception ex) {
+        JOptionPane.showMessageDialog(null, "Error al actualizar stock: " + ex.getMessage());
+    } finally {
+        cierreConexion();
     }
 
+    return actualizado;
+}
+
+    
+  private void cierreConexion() {
+        try {
+            conn.getMongo().close();
+        } catch (MongoException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cerrar la conexion");
+        }
+    }
 }
