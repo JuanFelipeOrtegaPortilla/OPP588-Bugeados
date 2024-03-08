e151f3d8f519e00dccb4427fa6bc8a21f5e0b24c
PIPE <PIPE@LAPTOP-UHNB891L>
2024-03-07 20:38:42 -0500

    2.1.8

---
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/modelo/Pedidos.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author PIPE
+ */
+public class Pedidos {
+    private int idProducto;
+    private String producto;
+    private int cantidad;
+    private double precio;
+    private double total;
+    private int idPedido;
+    private String fechaPedido;
+    private String fechaEntrega;
+
+    public Pedidos() {
+    }
+
+    public Pedidos(int idProducto, String producto, int cantidad, double precio, double total, int idPedido, String fechaPedido, String fechaEntrega) {
+        this.idProducto = idProducto;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.total = total;
+        this.idPedido = idPedido;
+        this.fechaPedido = fechaPedido;
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public Pedidos(int idProducto, String producto, int cantidad, double precio, double total, String fechaPedido, String fechaEntrega) {
+        this.idProducto = idProducto;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.total = total;
+        this.fechaPedido = fechaPedido;
+        this.fechaEntrega = fechaEntrega;
+    }
+    
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getFechaPedido() {
+        return fechaPedido;
+    }
+
+    public void setFechaPedido(String fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+
+    public String getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(String fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedidos{" + "idProducto=" + idProducto + ", producto=" + producto + ", cantidad=" + cantidad + ", precio=" + precio + ", total=" + total + ", idPedido=" + idPedido + ", fechaPedido=" + fechaPedido + ", fechaEntrega=" + fechaEntrega + '}';
+    }
+}
