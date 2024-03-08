080680649841a16d0f0184034e97a3cee3f18e24
PIPE <PIPE@LAPTOP-UHNB891L>
2024-03-07 21:16:04 -0500

    2.2.3

--- Proyecto_Panaderia/ProyectoFinalPanaderia/src/servicio/ProductoServicio.java
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/servicio/ProductoServicio.java
@@ -15,23 +15,23 @@ import modelo.Producto;
  */
 public class ProductoServicio {
 
-    public List<Producto> ListaProductos() {
+    public static List<Producto> ListaProductos() {
         return new MetodoProducto().ListaProductos();
     }
 
-    public boolean InsetarProductos(Producto producto) {
+    public static boolean InsetarProductos(Producto producto) {
         return new MetodoProducto().InsetarProductos(producto);
     }
 
-    public boolean ActualizarProductos(Producto producto) {
+    public static boolean ActualizarProductos(Producto producto) {
         return new MetodoProducto().ActualizarProductos(producto);
     }
 
-    public boolean EliminarProductos(int idProduto) {
+    public static boolean EliminarProductos(int idProduto) {
         return new MetodoProducto().EliminarProductos(idProduto);
     }
 
-    public Producto BuscarProductos(int idProducto) {
+    public static Producto BuscarProductos(int idProducto) {
         return new MetodoProducto().BuscarProductos(idProducto);
     }
 
