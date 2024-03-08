c9003b5d40034b4322559796b7b52d6fe9d1eb6d
PIPE <PIPE@LAPTOP-UHNB891L>
2024-03-07 20:38:19 -0500

    2.1.7

---
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/dao/MetodosPedidos.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import modelo.Pedidos;
+
+/**
+ *
+ * @author PIPE
+ */
+public class MetodosPedidos implements IPedidos{
+
+    @Override
+    public List<Pedidos> ListarPedidos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean InsetarPedido(Pedidos pedido) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean ActualizarPedidos(Pedidos pedido) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean EliminarPedido(int idPedido) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Pedidos BuscarIdPedido(int idPedido) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
