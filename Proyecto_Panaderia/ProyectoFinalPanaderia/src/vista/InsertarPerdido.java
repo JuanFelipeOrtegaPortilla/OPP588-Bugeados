cb96c0485a8dcf0bfb7ecae0a40a54ef841380a9
PIPE <PIPE@LAPTOP-UHNB891L>
2024-03-10 23:48:05 -0500

    2.3.10

--- Proyecto_Panaderia/ProyectoFinalPanaderia/src/vista/InsertarPerdido.java
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/vista/InsertarPerdido.java
@@ -364,6 +364,7 @@ public class InsertarPerdido extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         regresarConsultaPedidos();
+        dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void cmbIdProductoItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cmbIdProductoItemStateChanged
