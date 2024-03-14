800e2add464ee21fcc6a64aa8707db143b706063
PIPE <juanfelipeortegaportilla@gmail.com>
2024-03-14 00:27:36 -0500

    2.3.42

--- Proyecto_Panaderia/ProyectoFinalPanaderia/src/vistaExtras/InsertarExtra.java
+++ Proyecto_Panaderia/ProyectoFinalPanaderia/src/vistaExtras/InsertarExtra.java
@@ -312,6 +312,8 @@ public class InsertarExtra extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
